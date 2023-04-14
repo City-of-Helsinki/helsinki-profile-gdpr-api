b9737a40449d17eedee869834d49a4e9640daa2e
Aki Koskinen <aki@secondlion.fi>
2023-04-14 09:59:41 +0300

    Simplify tests for incorrect scopes
    The versions of the tests that used the correct scope were redundant
    because the correct scope is used in all the other tests. The point of
    these tests is to check what happens when an incorrect scope is used and
    that's what the tests now do.

--- tests/test_gdpr_api_query.py
+++ tests/test_gdpr_api_query.py
@@ -1,4 +1,3 @@
-import pytest
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
@@ -45,30 +44,19 @@ def test_user_can_only_access_his_own_profile(
     assert response.status_code == 403
 
 
-@pytest.mark.parametrize("use_correct_scope", [True, False])
-def test_gdpr_query_requires_correct_scope(
-    use_correct_scope, api_client, profile, requests_mock, settings
+def test_gdpr_query_requires_correct_query_scope(
+    api_client, profile, requests_mock, settings
 ):
-    if use_correct_scope:
-        auth_header = get_api_token_for_user_with_scopes(
-            profile.user,
-            [settings.GDPR_API_QUERY_SCOPE, settings.GDPR_API_DELETE_SCOPE],
-            requests_mock,
-        )
-    else:
-        auth_header = get_api_token_for_user_with_scopes(
-            profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-        )
+    auth_header = get_api_token_for_user_with_scopes(
+        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
+    )
     api_client.credentials(HTTP_AUTHORIZATION=auth_header)
 
     response = api_client.get(
         reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
     )
 
-    if use_correct_scope:
-        assert response.status_code == 200
-    else:
-        assert response.status_code == 403
+    assert response.status_code == 403
 
 
 def test_if_profile_not_found_return_204(
