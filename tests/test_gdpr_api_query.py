74564dcecc2c98f1999d7ae18795b6ec17da9e91
Aki Koskinen <aki@secondlion.fi>
2023-04-14 10:18:03 +0300

    Extract a helper method for executing the request in query tests
    There was a lot of repetition in the tests for the preparation for doing
    the query request.

--- tests/test_gdpr_api_query.py
+++ tests/test_gdpr_api_query.py
@@ -1,21 +1,26 @@
+import requests_mock
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from rest_framework.test import APIClient
 
 from tests.conftest import get_api_token_for_user_with_scopes
 
 User = get_user_model()
 
 
-def test_get_profile_information_from_gdpr_api(
-    api_client, profile, snapshot, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_QUERY_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.get(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
-    )
+def do_query(user, id_value, scopes=(settings.GDPR_API_QUERY_SCOPE,)):
+    api_client = APIClient()
+
+    with requests_mock.Mocker() as req_mock:
+        auth_header = get_api_token_for_user_with_scopes(user, scopes, req_mock)
+        api_client.credentials(HTTP_AUTHORIZATION=auth_header)
+
+        return api_client.get(reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": id_value}))
+
+
+def test_get_profile_information_from_gdpr_api(profile, snapshot):
+    response = do_query(profile.user, profile.id)
 
     assert response.status_code == 200
     snapshot.assert_match(response.json())
@@ -28,49 +33,21 @@ def test_gdpr_api_requires_authentication(api_client, profile):
     assert response.status_code == 401
 
 
-def test_user_can_only_access_his_own_profile(
-    api_client, user, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        user,
-        [settings.GDPR_API_QUERY_SCOPE, settings.GDPR_API_DELETE_SCOPE],
-        requests_mock,
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
+def test_user_can_only_access_his_own_profile(user, profile):
+    response = do_query(user, profile.id)
 
-    response = api_client.get(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
-    )
     assert response.status_code == 403
 
 
-def test_gdpr_query_requires_correct_query_scope(
-    api_client, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-
-    response = api_client.get(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
+def test_gdpr_query_requires_correct_query_scope(profile, settings):
+    response = do_query(
+        profile.user, profile.id, scopes=[settings.GDPR_API_DELETE_SCOPE]
     )
 
     assert response.status_code == 403
 
 
-def test_if_profile_not_found_return_204(
-    api_client, user, uuid_value, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        user, [settings.GDPR_API_QUERY_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.get(
-        reverse(
-            "helsinki_gdpr:gdpr_v1",
-            kwargs={"pk": uuid_value},
-        )
-    )
+def test_if_profile_not_found_return_204(user, uuid_value):
+    response = do_query(user, uuid_value)
 
     assert response.status_code == 204
