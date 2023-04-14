48dd6ad2be918a09fe61524a6ff6d491f8003eb7
Aki Koskinen <aki@secondlion.fi>
2023-04-14 10:03:56 +0300

    Extract a helper method for executing the request in delete tests
    There was a lot of repetition in the tests for the preparation for doing
    the delete request.

--- tests/test_gdpr_api_delete.py
+++ tests/test_gdpr_api_delete.py
@@ -1,5 +1,10 @@
+import urllib
+
+import requests_mock
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from rest_framework.test import APIClient
 
 from tests.conftest import get_api_token_for_user_with_scopes
 
@@ -8,48 +13,61 @@ from .models import Profile
 User = get_user_model()
 
 
-def test_delete_profile_dry_run_data(
-    true_value, api_client, profile, requests_mock, settings
+def do_delete(
+    user,
+    id_value,
+    scopes=(settings.GDPR_API_DELETE_SCOPE,),
+    query_params=None,
+    data=None,
 ):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id}),
-        data={"dry_run": true_value},
-        format="json",
-    )
+    api_client = APIClient()
+
+    with requests_mock.Mocker() as req_mock:
+        auth_header = get_api_token_for_user_with_scopes(user, scopes, req_mock)
+        api_client.credentials(HTTP_AUTHORIZATION=auth_header)
+
+        if query_params:
+            query = "?" + urllib.parse.urlencode(query_params)
+        else:
+            query = ""
+
+        request_kwargs = {"format": "json"}
+        if data:
+            request_kwargs["data"] = data
+
+        return api_client.delete(
+            reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": id_value}) + query,
+            **request_kwargs,
+        )
+
+
+def test_delete_profile_dry_run_data(true_value, profile):
+    response = do_delete(profile.user, profile.id, data={"dry_run": true_value})
 
     assert response.status_code == 204
     assert Profile.objects.count() == 1
     assert User.objects.count() == 1
 
 
-def test_delete_profile_dry_run_query_params(
-    true_value, api_client, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
-        + f"?dry_run={true_value}",
-    )
+def test_delete_profile_dry_run_query_params(true_value, profile):
+    response = do_delete(profile.user, profile.id, query_params={"dry_run": true_value})
 
     assert response.status_code == 204
     assert Profile.objects.count() == 1
     assert User.objects.count() == 1
 
 
-def test_delete_profile(api_client, profile, requests_mock, settings):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
+def test_delete_profile(profile):
+    response = do_delete(profile.user, profile.id)
+
+    assert response.status_code == 204
+    assert Profile.objects.count() == 0
+    assert User.objects.count() == 0
+
+
+def test_delete_profile_dry_run_query_params_false(false_value, profile):
+    response = do_delete(
+        profile.user, profile.id, query_params={"dry_run": false_value}
     )
 
     assert response.status_code == 204
@@ -57,35 +75,8 @@ def test_delete_profile(api_client, profile, requests_mock, settings):
     assert User.objects.count() == 0
 
 
-def test_delete_profile_dry_run_query_params_false(
-    false_value, api_client, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
-        + f"?dry_run={false_value}",
-    )
-
-    assert response.status_code == 204
-    assert Profile.objects.count() == 0
-    assert User.objects.count() == 0
-
-
-def test_delete_profile_dry_run_data_false(
-    false_value, api_client, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id}),
-        data={"dry_run": false_value},
-        format="json",
-    )
+def test_delete_profile_dry_run_data_false(false_value, profile):
+    response = do_delete(profile.user, profile.id, data={"dry_run": false_value})
 
     assert response.status_code == 204
     assert Profile.objects.count() == 0
@@ -99,32 +90,15 @@ def test_gdpr_api_requires_authentication(api_client, profile):
     assert response.status_code == 401
 
 
-def test_user_can_only_access_his_own_profile(
-    api_client, user, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        user,
-        [settings.GDPR_API_QUERY_SCOPE, settings.GDPR_API_DELETE_SCOPE],
-        requests_mock,
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
+def test_user_can_only_access_his_own_profile(user, profile):
+    response = do_delete(user, profile.id)
 
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
-    )
     assert response.status_code == 403
 
 
-def test_gdpr_delete_requires_correct_delete_scope(
-    api_client, profile, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        profile.user, [settings.GDPR_API_QUERY_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-
-    response = api_client.delete(
-        reverse("helsinki_gdpr:gdpr_v1", kwargs={"pk": profile.id})
+def test_gdpr_delete_requires_correct_delete_scope(profile, settings):
+    response = do_delete(
+        profile.user, profile.id, scopes=[settings.GDPR_API_QUERY_SCOPE]
     )
 
     assert response.status_code == 403
@@ -132,18 +106,7 @@ def test_gdpr_delete_requires_correct_delete_scope(
     assert User.objects.count() == 1
 
 
-def test_if_profile_not_found_return_204(
-    api_client, user, uuid_value, requests_mock, settings
-):
-    auth_header = get_api_token_for_user_with_scopes(
-        user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
-    )
-    api_client.credentials(HTTP_AUTHORIZATION=auth_header)
-    response = api_client.delete(
-        reverse(
-            "helsinki_gdpr:gdpr_v1",
-            kwargs={"pk": uuid_value},
-        )
-    )
+def test_if_profile_not_found_return_204(user, uuid_value):
+    response = do_delete(user, uuid_value)
 
     assert response.status_code == 204
